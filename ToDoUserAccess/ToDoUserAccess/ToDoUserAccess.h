6af32fc850d2f80a317925f97dcb4c3df67f8fb4
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-10-26 17:54:13 -0700

    Add User Access Framework for sign up and sign in features.

---
+++ ToDoUserAccess/ToDoUserAccess/ToDoUserAccess.h
@@ -0,0 +1,18 @@
+//
+//  ToDoUserAccess.h
+//  ToDoUserAccess
+//
+//  Created by Deniz Tutuncu on 10/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToDoUserAccess.
+FOUNDATION_EXPORT double ToDoUserAccessVersionNumber;
+
+//! Project version string for ToDoUserAccess.
+FOUNDATION_EXPORT const unsigned char ToDoUserAccessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToDoUserAccess/PublicHeader.h>
+
+
