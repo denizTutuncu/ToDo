c6a5588016f1f6ec1b471ae4ffeaff090a4d38a0
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-11-10 16:06:51 -0800

    Add `LogInFromRemoteUseCaseTests` and `RemoteLoginService`.

---
+++ ToDoUserAccess/ToDoUserAccess/Authentication API/Log In API/RemoteLoginService.swift
@@ -0,0 +1,10 @@
+//
+//  RemoteLoginService.swift
+//  ToDoUserAccess
+//
+//  Created by Deniz Tutuncu on 11/10/21.
+//
+
+import Foundation
+
+public final class RemoteLoginService {}
---
+++ ToDoUserAccess/ToDoUserAccessTests/LogInFromRemoteUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  LogInFromRemoteUseCaseTests.swift
+//  ToDoUserAccessTests
+//
+//  Created by Deniz Tutuncu on 11/10/21.
+//
+
+import XCTest
+
+class LogInFromRemoteUseCaseTests: XCTestCase {
+    
+}
